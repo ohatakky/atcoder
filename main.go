272c9d7d825fcc230bfa19a13342ae43a9828e27
Akira Otaki <ohatakky@gmail.com>
2020-04-26 18:02:30 +0900

    tmp

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func chmax(x *int, y int) bool {
 	return false
 }
 
+// 最大公約数
 func gcd(a, b int) int {
 	if b == 0 {
 		return a
@@ -57,19 +58,50 @@ func gcd(a, b int) int {
 	return gcd(b, a%b)
 }
 
+func comb(n, k int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD
+}
+
 /*-------------------init-------------------*/
 
 const (
 	INF = 1000000000000000000
 	MOD = 1e9 + 7
+
+	COMB_MAX = 510000
 )
 
 var (
 	readString func() string
 )
 
+var (
+	fac  = [COMB_MAX]int{}
+	finv = [COMB_MAX]int{}
+	inv  = [COMB_MAX]int{}
+)
+
 func init() {
 	readString = newReadString(os.Stdin)
+}
+
+func combInit() {
+	fac[0] = 1
+	fac[1] = 1
+	finv[0] = 1
+	finv[1] = 1
+	inv[1] = 1
+	for i := 2; i < COMB_MAX; i++ {
+		fac[i] = fac[i-1] * i % MOD
+		inv[i] = MOD - inv[MOD%i]*(MOD/i)%MOD
+		finv[i] = finv[i-1] * inv[i] % MOD
+	}
 }
 
 /*-------------------inputs-------------------*/
