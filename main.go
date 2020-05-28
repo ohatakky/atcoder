fe6b15f1dd88ccf224fa68045b17eea79ac2fb1d
Akira Otaki <ohatakky@gmail.com>
2020-05-28 20:25:42 +0900

    tmp

--- main.go
+++ main.go
@@ -100,6 +100,18 @@ func permutations(arr []int) [][]int {
 	return res
 }
 
+func isPrime(n int) bool {
+	if n == 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func divisor(n int) (res []int) {
 	for i := 1; i*i <= n; i++ {
 		if n%i == 0 {
