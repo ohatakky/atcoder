1915ca5b8c09d1122d00d7460b85917070d688fb
Akira Otaki <ohatakky@gmail.com>
2020-05-31 17:40:01 +0900

    tmp

--- temp20200531_173913/main.go
+++ temp20200531_173913/main.go
@@ -62,6 +62,16 @@ func lcm(a, b int) int {
 	return (a * b) / gcd(a, b)
 }
 
+func comb(n, k int) int {
+	if n < k {
+		return 0
+	}
+	if n < 0 || k < 0 {
+		return 0
+	}
+	return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD
+}
+
 func permutations(arr []int) [][]int {
 	var helper func([]int, int)
 	res := [][]int{}
@@ -120,14 +130,35 @@ func divisor(n int) (res []int) {
 const (
 	INF = 1000000000000000000
 	MOD = 1e9 + 7
+
+	COMB_MAX = 510000
 )
 
 var (
 	readString func() string
 )
 
+var (
+	fac  = [COMB_MAX]int{}
+	finv = [COMB_MAX]int{}
+	inv  = [COMB_MAX]int{}
+)
+
 func init() {
 	readString = newReadString(os.Stdin)
+}
+
+func combInit() {
+	fac[0] = 1
+	fac[1] = 1
+	finv[0] = 1
+	finv[1] = 1
+	inv[1] = 1
+	for i := 2; i < COMB_MAX; i++ {
+		fac[i] = fac[i-1] * i % MOD
+		inv[i] = MOD - inv[MOD%i]*(MOD/i)%MOD
+		finv[i] = finv[i-1] * inv[i] % MOD
+	}
 }
 
 /*-------------------inputs-------------------*/
